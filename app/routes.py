de87ff2d1c7e221af5d4c01cfd15f1c8e6ced1f4
KPalos <kyla.palos@gmail.com>
2018-11-10 02:38:07 -0500

    finished creating database for users and posts

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,6 @@
+'''
+Routes module
+'''
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms  import LoginForm
@@ -5,14 +8,17 @@ from app.forms  import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
+    '''
+    Index page of the website
+    '''
     user = {'username': 'Miguel'}
     posts = [
         {
-            'author': { 'username': 'John' },
+            'author': {'username': 'John'},
             'body': 'Beautiful day in Portland!'
         },
         {
-            'author': { 'username': 'John' },
+            'author': {'username': 'John'},
             'body': 'The Avengers movie was so cool!'
         }
     ]
@@ -20,6 +26,9 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    '''
+    Login page of the website
+    '''
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, remember me={}'.format(
@@ -27,4 +36,3 @@ def login():
         ))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
-
--- microblog.py
+++ microblog.py
@@ -1,1 +1,12 @@
-from app import app
+'''
+Microblog Website
+'''
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+  '''
+  Function to create shell context
+  '''
+  return {'db': db, 'User': User, 'Post': Post}
