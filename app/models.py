4ec16147d1edea628ec6a356e68c45be19d7df63
KPalos <kyla.palos@gmail.com>
2018-11-12 12:56:12 -0500

    finished creating user profile with avatars

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ Database Module
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 from app import db, login
 
 @login.user_loader
@@ -23,6 +24,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -38,6 +41,10 @@ class User(UserMixin, db.Model):
         Checking to see if password provided matches stored hashed password
         '''
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
 class Post(db.Model):
     '''
--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,9 @@ Routes module
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
+from datetime import datetime
 from app import app, db
-from app.forms  import LoginForm, RegistrationForm
+from app.forms  import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 
 @app.route('/')
@@ -48,7 +49,7 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         ## app would remember that the user is logged in
-        ## by setting current_user to current_user
+        ## by setting current_user to currently logged in user
         login_user(user, remember=form.remember_me.data)
         ## gets the url path assigned to the 'next' argument in the url
         next_page = request.args.get('next')
@@ -84,3 +85,38 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        ## no db.session.add() because whenever the current_user is referenced,
+        ## Flask automatically queries the db that puts the target user
+        ## in the database session. Since last_seen is already a filled column,
+        ## there's no need to add it anymore (this is just a simple modification)
+        db.session.commit()
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
